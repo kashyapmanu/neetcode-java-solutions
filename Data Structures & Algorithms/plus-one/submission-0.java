97cf3f2ee0437df2161fd32f1ff9cc26dbd32ea9
Manukashyap.U.V <manukashyap.u.v@gmail.com>
2026-05-06 07:38:39 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else if (digits[i] == 9) {
+                digits[i] = 0;
+            }
+        }
+        if (digits[0] == 0) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            for (int i = 0; i < digits.length; i++) {
+                result[i + 1] = digits[i];
+            }
+            return result;
+        }
+        return null;
+    }
+}
